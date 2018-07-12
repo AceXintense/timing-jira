916b3866682fd7ace0d72c191ac0a18456cb6f2e
Sam Grew <shanker1210@hotmail.co.uk>
2018-07-12 22:46:07 +0100

    Added support for multiple files which can be loaded into the app.

--- uploadToJIRA.php
+++ uploadToJIRA.php
@@ -3,34 +3,44 @@
 use Core\Authentication;
 use Core\Console;
 use Core\CSVParser;
+use Core\Files;
 use Core\JIRA;
 use Models\Worklog;
 include 'vendor/autoload.php';
 
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
+
 /*
- * Initial script startup setting JIRA Login, URL, CSV Data.
+ * Initial setup JIRA Login, Data file loading.
  */
-CSVParser::getInstance()->load('./data/Timing Export.csv');
 Authentication::setUsernameAndPassword(getenv('JIRA_USERNAME'), getenv('JIRA_PASSWORD'), getenv('JIRA_PASSWORD_BASE64'));
 JIRA::setURL(getenv('JIRA_URL'));
 
-try {
-    //Loop through each row on the CSV and upload the Data.
-    foreach (CSVParser::getInstance()->format()->getFormattedRows() as $formattedRow) {
-        JIRA::addWorklog(
-            $formattedRow['Task Title'],
-            new Worklog(
+/*
+ * Loops through all the files in the directory specified in the config.
+ */
+foreach (Files::getInstance()->getFilesInDirectory(getenv('DATA_DIRECTORY')) as $file) {
+
+    CSVParser::getInstance()->load(getenv('DATA_DIRECTORY') . $file);
+
+    try {
+        //Loop through each row on the CSV and upload the Data.
+        foreach (CSVParser::getInstance()->format()->getFormattedRows() as $formattedRow) {
+            JIRA::addWorklog(
                 $formattedRow['Task Title'],
-                JIRA::formatDate($formattedRow['Start Date']),
-                (float)$formattedRow['Duration'],
-                $formattedRow['Notes']
-            )
-        );
+                new Worklog(
+                    $formattedRow['Task Title'],
+                    JIRA::formatDate($formattedRow['Start Date']),
+                    (float)$formattedRow['Duration'],
+                    $formattedRow['Notes']
+                )
+            );
+        }
+    } catch (Exception $exception) {
+        Console::log($exception->getMessage());
+    } catch (\GuzzleHttp\Exception\GuzzleException $exception) {
+        Console::log($exception->getMessage());
     }
-} catch (Exception $exception) {
-    Console::log($exception->getMessage());
-} catch (\GuzzleHttp\Exception\GuzzleException $exception) {
-    Console::log($exception->getMessage());
+
 }
